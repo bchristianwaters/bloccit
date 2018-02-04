ace994bdae1af8f21cf92d685590de2be50d5808
EC2 Default User <ec2-user@ip-172-31-11-194.us-east-2.compute.internal>
2018-02-04 05:37:58 +0000

    moderators

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,8 @@
  class TopicsController < ApplicationController
    
    before_action :require_sign_in, except: [:index, :show]
-   before_action :authorize_user, except: [:index, :show]
+   before_action :authorize_user1, only: [:new, :create, :destroy]
+   before_action :authorize_user2, only: [:edit, :update]
    
    def index
      @topics = Topic.all
@@ -60,9 +61,16 @@
      params.require(:topic).permit(:name, :description, :public)
    end
    
-   def authorize_user
+   def authorize_user1
      unless current_user.admin?
        flash[:alert] = "You must be an admin to do that."
+       redirect_to topics_path
+     end
+   end
+   
+   def authorize_user2
+     unless (current_user.admin? || current_user.moderator?)
+       flash[:alert] = "You must be an admin or moderator to do that."
        redirect_to topics_path
      end
    end
