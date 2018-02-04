a240b809151734ea1c63a53f751839793109b05e
EC2 Default User <ec2-user@ip-172-31-11-194.us-east-2.compute.internal>
2018-02-04 04:47:01 +0000

    user roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,9 +38,17 @@
    )
  end
  
- user = User.first
- user.update_attributes!(
-   email: 'christianwaters1993@gmail.com',
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'password',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'password'
  )
  
